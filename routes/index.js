a2eaf2d32f0978e6999ad6db3b12cae78b6205eb
Sohaib Riaz <sohaib@paynest.com>
2022-12-15 22:45:50 +0500

    session fix

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ module.exports = () => {
     });
 
     router.get('/home', (req, res) => {
-        if (req.session.admin) {
+        if (req.session.admin != undefined) {
             if(req.session.admin.role === Type.ROOT){
                 getAll().then((all_employees) => {
                     getAllUsers().then((all_users) =>{
